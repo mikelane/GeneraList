b3f84a3901ff352a471df6e7d8f018ee665fde58
Michael Lane <mikelane@gmail.com>
2016-11-19 06:45:50 -0800

    Cleans up code starts adding stubs

--- main.py
+++ main.py
@@ -12,19 +12,18 @@ To add: Music Playlists, Video Playlists, Lists of slides for a slideshow. These
 interact with other web services.
 """
 
-__author__ = "Mike Lane"
-__email__ = "mikelane@gmail.com"
-
 from __future__ import print_function
-import json
-import boto3
+
+
+__author__ = 'Mike Lane'
+__email__ = 'mikelane@gmail.com'
 
 # Set APP_ID to ensure only your skill calls this lambda handler
-APP_ID = "amzn1.ask.skill.e208302c-710b-4f30-9344-d0ae6ca3b774"
-SKILL_NAME = "GeneralList"
-SKILL_INVOKE = "generalist"
-DB_TABLENAME = "lists"
-DB_REGION = "us-east-1"
+APP_ID = 'amzn1.ask.skill.e208302c-710b-4f30-9344-d0ae6ca3b774'
+SKILL_NAME = 'GeneralList'
+SKILL_INVOKE = 'generalist'
+DB_TABLENAME = 'lists'
+DB_REGION = 'us-east-1'
 
 
 # --------------- Main handler ------------------
@@ -34,23 +33,23 @@ def lambda_handler(event, context):
     Returns a JSON response to Alexa with:
     outputSpeech, reprompt, card, shouldEndSession"""
 
-    if (event["session"]["application"]["applicationId"]
+    if (event['session']['application']['applicationId']
             != 'amzn1.ask.skill.e208302c-710b-4f30-9344-d0ae6ca3b774'):
-        if (APP_ID != ""):
-            if (event["session"]["application"]["applicationId"] != APP_ID):
-                raise ValueError("Invalid Application ID",
-                                 event["session"]["application"]["applicationId"],
+        if APP_ID != '':
+            if event['session']['application']['applicationId'] != APP_ID:
+                raise ValueError('Invalid Application ID',
+                                 event['session']['application']['applicationId'],
                                  APP_ID)
 
-    if event["session"]["new"]:
-        on_session_started({"requestId": event["request"]["requestId"]}, event["session"])
+    if event['session']['new']:
+        on_session_started({'requestId': event['request']['requestId']}, event['session'])
 
-    if event["request"]["type"] == "LaunchRequest":
-        return on_launch(event["request"], event["session"])
-    elif event["request"]["type"] == "IntentRequest":
-        return on_intent(event["request"], event["session"])
-    elif event["request"]["type"] == "SessionEndedRequest":
-        return on_session_ended(event["request"], event["session"])
+    if event['request']['type'] == 'LaunchRequest':
+        return on_launch(event['request'], event['session'])
+    elif event['request']['type'] == 'IntentRequest':
+        return on_intent(event['request'], event['session'])
+    elif event['request']['type'] == 'SessionEndedRequest':
+        return on_session_ended(event['request'], event['session'])
 
 
 # --------------- Helpers that build all of the responses ----------------------
@@ -63,8 +62,8 @@ def build_speechlet_response(title, output, reprompt_text, should_end_session):
         },
         'card': {
             'type': 'Simple',
-            'title': "SessionSpeechlet - " + title,
-            'content': "SessionSpeechlet - " + output
+            'title': 'SessionSpeechlet - ' + title,
+            'content': 'SessionSpeechlet - ' + output
         },
         'reprompt': {
             'outputSpeech': {
@@ -90,36 +89,47 @@ def get_welcome_response():
     """ If we wanted to initialize the session to have some attributes we could add those here"""
     session_attributes = {}
 
-    card_title = "Welcome to GeneraList"
-    speech_output = """Welcome to generalist. Do you want to create, edit, or access a list?"""
+    card_title = 'Welcome to GeneraList'
+    speech_output = 'Welcome to generalist. Do you want to create, edit, or access a list?'
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
-    reprompt_text = """I didn't get that. Say create, edit, or access."""
+    reprompt_text = "I didn't get that. Say create, edit, or access."
     should_end_session = False
-    return build_response(session_attributes,
-                          build_speechlet_response(card_title,
-                                                   speech_output,
-                                                   reprompt_text,
-                                                   should_end_session))
+    return build_response(session_attributes=session_attributes,
+                          speechlet_response=build_speechlet_response(title=card_title,
+                                                                      output=speech_output,
+                                                                      reprompt_text=reprompt_text,
+                                                                      should_end_session=should_end_session))
 
 
 def handle_session_end_request():
-    card_title = "Session Ended"
-    speech_output = """Goodbye."""
+    card_title = 'Session Ended'
+    speech_output = 'Goodbye.'
     # Setting this to true ends the session and exits the skill.
     should_end_session = True
-    return build_response({},
-                          build_speechlet_response(card_title,
-                                                   speech_output,
-                                                   None,
-                                                   should_end_session))
+    return build_response(session_attributes={},
+                          speechlet_response=build_speechlet_response(title=card_title,
+                                                                      output=speech_output,
+                                                                      reprompt_text=None,
+                                                                      should_end_session=should_end_session))
 
 
 def create_list(intent, session):
     """Use the user id to create a new list. Tell the user to simply speak the steps. After each step,
     validate and ask if they want to add another step or stop. If the list already exists, just error out
     with a message that tells the user to modify a list instead."""
-    return None
+    card_title = intent['name']
+    session_attributes = {}
+    should_end_session = True  # TODO change this to false
+    statuses = []
+    reprompt_text = None
+
+    speech_output = "I'm working on that feature."  # TODO change this
+    return build_response(session_attributes=session_attributes,
+                          speechlet_response=build_speechlet_response(title=card_title,
+                                                                      output=speech_output,
+                                                                      reprompt_text=reprompt_text,
+                                                                      should_end_session=should_end_session))
 
 
 def delete_list(intent, session):
@@ -164,7 +174,7 @@ def get_prev_item_from_list(intent, session):
 def peek_at_prev_item_from_list(intent, session):
     """Use the user id to determine the current list and step. Execute the step but don't change the
     current step number. This is kind of like a reminder. It's more like 'Hey, what was the last step?'
-    instead of 'Go back to the next step.'"""
+    instead of 'Go back to the next step."""
     return None
 
 
@@ -185,42 +195,42 @@ def cancel(intent, sessions):
 def on_session_started(session_started_request, session):
     """ Called when the session starts """
 
-    print("on_session_started requestId={}, sessionId={}".format(session_started_request['requestId'],
+    print('on_session_started requestId={}, sessionId={}'.format(session_started_request['requestId'],
                                                                  session['sessionId']))
 
 
 def on_launch(launch_request, session):
     """ Called when the user launches the skill without specifying what they want"""
-    print("on_launch requestId={}, sessionId={}".format(launch_request['requestId'], session['sessionId']))
+    print('on_launch requestId={}, sessionId={}'.format(launch_request['requestId'], session['sessionId']))
     # Dispatch to your skill's launch
     return get_welcome_response()
 
 
 def on_intent(intent_request, session):
     """ Called when the user specifies an intent for this skill """
-    print("on_intent requestId={}, sessionId={}".format(intent_request['requestId'], session['sessionId']))
+    print('on_intent requestId={}, sessionId={}'.format(intent_request['requestId'], session['sessionId']))
 
     intent = intent_request['intent']
     intent_name = intent_request['intent']['name']
 
     # Dispatch to your skill's intent handlers
-    if intent_name == "LoadListIntent":
+    if intent_name == 'LoadListIntent':
         return load_list(intent, session)
-    elif intent_name == "CreateListIntent":
+    elif intent_name == 'CreateListIntent':
         return create_list(intent, session)
-    elif intent_name == "AMAZON.NextIntent":
+    elif intent_name == 'AMAZON.NextIntent':
         return get_next_item_from_list(intent, session)
-    elif intent_name == "AMAZON.HelpIntent":
+    elif intent_name == 'AMAZON.HelpIntent':
         return get_welcome_response()
-    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+    elif intent_name == 'AMAZON.CancelIntent' or intent_name == 'AMAZON.StopIntent':
         return handle_session_end_request()
     else:
-        raise ValueError("Invalid intent")
+        raise ValueError('Invalid intent')
 
 
 def on_session_ended(session_ended_request, session):
     """ Called when the user ends the session.
     Is not called when the skill returns should_end_session=true"""
-    print("on_session_ended requestId={}, sessionId={}".format(session_ended_request['requestId'],
+    print('on_session_ended requestId={}, sessionId={}'.format(session_ended_request['requestId'],
                                                                session['sessionId']))
     # add cleanup logic here
